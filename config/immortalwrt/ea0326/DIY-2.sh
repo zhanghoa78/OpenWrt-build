7c1112d5c863e8d80d84058cc7537f22169da24f
zhanghoa78 <zhanghoa78@gmail.com>
2025-11-07 10:36:48 +0800

    Update DIY-2.sh

--- config/immortalwrt/ea0326/DIY-2.sh
+++ config/immortalwrt/ea0326/DIY-2.sh
@@ -2,14 +2,14 @@
 #============================================================================================
 
 # 1-设置管理地址 (注意：源 IP 已从 192.168.6.1 改为 192.168.1.1)
-sed -i 's/192.168.1.1/192.168.2.223/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.223/g' package/base-files/files/bin/config_generate
 
 # 2-编译内核版本 (已注释)
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/mediatek/Makefile
 
 # 3-添加固件日期 (应该有效)
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+#sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
 
 # 4-增固件连接数 (应该有效，注意内存)
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
